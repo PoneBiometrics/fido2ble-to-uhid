94d9aba4603419f1f1769e7cd344dfe7139803d7
Kacper Wysocki <kaw@ponebiometrics.com>
2025-04-03 14:11:51 +0200

    Rename project to `fido2ble`
    Make the name easier to pronounce and remember by simplifying the name.

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
 
-setup(name='fido2ble-to-uhid',
+setup(name='fido2ble',
       version='0.0.1',
       description='',
       author='Jó Ágila Bitsch',
       author_email='jo.bitsch@gmail.com',
-      url='https://github.com/PoneBiometrics/fido2ble-to-uhid/',
+      url='https://github.com/PoneBiometrics/fido2ble/',
       packages=find_packages(),
       entry_points={
           'console_scripts': [
-              'fido2ble-to-uhid=fido2ble_to_uhid.fido2ble_to_uhid:main'
+              'fido2ble=fido2ble.fido2ble:main'
           ]
       }
      )
